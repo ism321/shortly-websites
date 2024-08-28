21bd4e614c05fed1e8a12300a73c587e185f85c0
ism321 <ismailikkhankhan786@gmail.com>
2024-08-28 19:26:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        cyan:'hsl(180, 66%, 49%)',
+        cyanlight:'hsl(180, 66%, 69%)',
+        darkviolet:'hsl(257, 27%, 26%)',
+        red:'hsl(0, 87%, 67%)',
+        grayishviolet:'hsl(257, 7%, 63%)',
+        verydarkblue:'hsl(255, 11%, 22%)',
+        verydarkviolet:'hsl(260, 8%, 14%)',
+      },
+      fontfamily: {
+        sans: ['poppins','sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
+    
+      },
+    },
+  
+  plugins: [],
+  }
